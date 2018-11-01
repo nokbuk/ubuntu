df45a0b921c195c6c72b800bbd5bac5e822a444c
Marcos Paulo da Costa <44422455+nokbuk@users.noreply.github.com>
2018-11-01 20:09:16 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#Meu Primeiro Script
+#!/bin/bash
+
+#Listando os Diretorios do Linux
+ls -lha /
+read
+clear
+
+#atualizando o Apt ou Apt-Get
+apt update
+echo "Listas atualizadas!!!"
+read
+cls
+
+#desligando o servidor
+shutdown -h now
